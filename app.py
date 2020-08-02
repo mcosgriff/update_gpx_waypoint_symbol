b81344f354ce27386bdb742b77d66f0848d67112
Matthew J. Cosgriff <mcosgriff@gmail.com>
2020-08-02 07:16:34 -0600

    Updating GPX Waypoint symbol to use Locus Maps number icon

---
+++ app.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+from typing import Iterator
+
+import gpxpy
+
+
+def parse_cmd_arguments() -> argparse.Namespace:
+    args = argparse.ArgumentParser()
+    args.add_argument("--working-directory", type=str, help='Directory to look for gpx files', required=True)
+
+    return args.parse_args()
+
+
+def find_gpx_files(working_dir: str) -> Iterator[str]:
+    for file in os.listdir(working_dir):
+        if file.endswith(".gpx"):
+            yield os.path.join(working_dir, file)
+
+
+def get_locus_icon(number: int) -> str:
+    return f'file:Locus Misc.zip:number_{number}.png'
+
+
+def update_gpx(gpx_file_path: str) -> gpxpy.gpx:
+    with open(gpx_file_path, 'r', encoding='utf-8') as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+        waypoint: gpx.GPXWaypoint
+        for number, waypoint in enumerate(gpx.waypoints, start=1):
+            waypoint.symbol = get_locus_icon(number)
+
+        return gpx
+
+
+def main() -> None:
+    args = parse_cmd_arguments()
+
+    for gpx_file in find_gpx_files(args.working_directory):
+        updated_gpx_file = update_gpx(gpx_file)
+
+        with open(gpx_file, 'w', encoding='utf-8') as gpx_fp:
+            gpx_fp.write(updated_gpx_file.to_xml())
+            print(f'Updated {gpx_file}')
+
+
+if __name__ == '__main__':
+    main()
